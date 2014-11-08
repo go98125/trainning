7bdab6ded4d96e93694616c161de7a583a705622
go98125 <cl50404@yahoo.com.tw>
2014-11-08 22:09:44 +0800

    KO

--- test-app/app/controllers/groups_controller.rb
+++ test-app/app/controllers/groups_controller.rb
@@ -9,11 +9,44 @@ class GroupsController < ApplicationController
 		@group=Group.new
 	end
 	def create
-		@group=Group.new(params[:group])
-		if @group.save
+		@group=Group.new(group_params)
+		@group.save
+		redirect_to groups_path
+	end
+	def edit
+		@group=Group.find(params[:id])
+	end
+	def update 
+		@group=Group.find(params[:id])
+		if @group.update(group_params)
+			redirect_to group_path(@group)
+			#redirect_to groups_path
+		else
+			render :edit
+		end
+	end
+	def update
+		@group=Group.find(params[:id])
+		if @group.update(group_params)
+			#redirect_to group_path(@group)
 			redirect_to groups_path
 		else
-			render :new
+			render :edit
 		end
+	end
+	def destroy
+		@group=Group.find(params[:id])
+		@group.destroy
+		redirect_to groups_path
+	end
+
+
+
+
+
+	private
+
+	def group_params
+		params.require(:group).permit(:title, :description)
 	end
 end
